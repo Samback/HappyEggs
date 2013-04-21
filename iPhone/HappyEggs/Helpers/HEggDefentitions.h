c3dadd7093e52ed7dda4d21caf5915ee611aad2d
Max Tymchii <samback@ukr.net>
2013-04-21 14:06:03 +0300

    Implement sharing via social networks.

--- iPhone/HappyEggs/Helpers/HEggDefentitions.h
+++ iPhone/HappyEggs/Helpers/HEggDefentitions.h
@@ -4,3 +4,10 @@
 
 #define BASE_URL @"http://ukr.net"
 #define BUMP_API_KEY @"de703e6680454adbbf3d1ac99727c9b0"
+#define VK_APP_ID @"3592400"
+
+#define SHARING_IMAGE @"sharing_image.png"
+#define SHARING_URL_FOR_APP @"http://ukr.net"
+#define SHARING_TEXT NSLocalizedString(@"Мне понравилось это приложение, сыграй со мной :)", @"Text for sharing")
+
+#define GA_API_KEY @"UA-38756409-2"
